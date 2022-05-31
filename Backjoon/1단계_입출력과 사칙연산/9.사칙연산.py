22d75f30a9f006697305e2f6e25e1711f055bc9d
unknown <akskejdtod@gmail.com>
2022-05-31 11:52:22 +0900

    2022/05/31

---
+++ Backjoon/1단계_입출력과 사칙연산/9.사칙연산.py
@@ -0,0 +1,31 @@
+(A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+
+(A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+
+세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+
+
+
+
+
+
+A = 5
+B = 8
+C = 4
+int = A+B % C
+print(A+B % C)
+A = 2
+B = 1
+int = A-B
+print(A-B)
+A = 10
+B= 0
+int = A*B
+print(A * B)
+A = 50
+B = 0.5
+int = A/B
+print(A / B)
+
+
+
